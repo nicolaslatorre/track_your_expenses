f3bbdf5ea4ba95c4a032b42a5014e905ea65e73d
nicolaslatorre <nicolas.latorre26@gmail.com>
2021-02-24 06:55:56 +0100

    feat: retrieve expenses given a month and a year

--- app/tracker.py
+++ app/tracker.py
@@ -1,9 +1,19 @@
+import datetime
+
 class Tracker(object):
     expenses = dict()
 
     def __init__(self):
         pass
 
-    def addExpense(self, expense):
+    def add_expense(self, expense):
         date = expense.date.__str__()
         self.expenses[date] = expense
+
+    def get_expenses_by_month_and_year(self, month, year):
+        return dict(filter(
+            lambda elem:
+            datetime.date.fromisoformat(elem[0]).month == month and
+            datetime.date.fromisoformat(elem[0]).year == year,
+            self.expenses.items()
+        ))
--- test/test_track_your_expenses.py
+++ test/test_track_your_expenses.py
@@ -7,6 +7,43 @@ class TestTrackYourExpenses(unittest.TestCase):
     def test_should_add_a_new_expense(self):
         tracker = Tracker()
         expense = Expense('spesa cibo', 12, datetime.date(2021, 2, 23))
-        tracker.addExpense(expense)
+        tracker.add_expense(expense)
         self.assertEqual(tracker.expenses['2021-02-23'], expense)
+
+    def test_should_retrieve_expenses_for_a_specific_month_number_2(self):
+        tracker = Tracker()
+        expense1 = Expense('cibo', 40, datetime.date(2021, 2, 14))
+        expense2 = Expense('spesa cibo', 12, datetime.date(2021, 2, 23))
+        expense3 = Expense('spesa cibo', 12, datetime.date(2021, 3, 3))
+        tracker.add_expense(expense1)
+        tracker.add_expense(expense2)
+        tracker.add_expense(expense3)
+        expenses = tracker.get_expenses_by_month_and_year(2, 2021)
+        self.assertEqual(True, expense1 in expenses.values())
+        self.assertEqual(True, expense2 in expenses.values())
+        self.assertEqual(False, expense3 in expenses.values())
+
+    def test_should_retrieve_expenses_for_a_specific_month_number_3(self):
+        tracker = Tracker()
+        expense1 = Expense('cibo', 40, datetime.date(2021, 2, 14))
+        expense2 = Expense('spesa cibo', 12, datetime.date(2021, 2, 23))
+        expense3 = Expense('spesa cibo', 12, datetime.date(2021, 3, 3))
+        tracker.add_expense(expense1)
+        tracker.add_expense(expense2)
+        tracker.add_expense(expense3)
+        expenses = tracker.get_expenses_by_month_and_year(3, 2021)
+        self.assertEqual(False, expense1 in expenses.values())
+        self.assertEqual(False, expense2 in expenses.values())
+        self.assertEqual(True, expense3 in expenses.values())
+
+    def test_should_return_an_empty_dictionary_if_no_expenses_are_available_for_a_specific_month(self):
+        tracker = Tracker()
+        expense1 = Expense('cibo', 40, datetime.date(2021, 2, 14))
+        expense2 = Expense('spesa cibo', 12, datetime.date(2021, 2, 23))
+        expense3 = Expense('spesa cibo', 12, datetime.date(2021, 3, 3))
+        tracker.add_expense(expense1)
+        tracker.add_expense(expense2)
+        tracker.add_expense(expense3)
+        expenses = tracker.get_expenses_by_month_and_year(4, 2021)
+        self.assertEqual(True, len(expenses.keys()) == 0)
 
