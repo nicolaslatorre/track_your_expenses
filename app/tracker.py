c68fa83155501c000433c6d57a939ecfb4ad58f1
nicolaslatorre <nicolas.latorre26@gmail.com>
2021-02-25 06:44:34 +0100

    feat: group expenses by category

--- app/tracker.py
+++ app/tracker.py
@@ -17,3 +17,12 @@ class Tracker(object):
             datetime.date.fromisoformat(elem[0]).year == year,
             self.expenses.items()
         ))
+
+    def group_expenses_by_category(self, expenses):
+        grouped_expenses = dict()
+        for key, value in expenses.items():
+            if value.category not in grouped_expenses:
+                grouped_expenses[value.category] = [value]
+            else:
+                grouped_expenses[value.category].append(value)
+        return grouped_expenses
--- test/test_track_your_expenses.py
+++ test/test_track_your_expenses.py
@@ -6,15 +6,15 @@ from app.expense import Expense
 class TestTrackYourExpenses(unittest.TestCase):
     def test_should_add_a_new_expense(self):
         tracker = Tracker()
-        expense = Expense('spesa cibo', 12, datetime.date(2021, 2, 23))
+        expense = Expense("spesa cibo", 12, datetime.date(2021, 2, 23))
         tracker.add_expense(expense)
-        self.assertEqual(tracker.expenses['2021-02-23'], expense)
+        self.assertEqual(tracker.expenses["2021-02-23"], expense)
 
     def test_should_retrieve_expenses_for_a_specific_month_number_2(self):
         tracker = Tracker()
-        expense1 = Expense('cibo', 40, datetime.date(2021, 2, 14))
-        expense2 = Expense('spesa cibo', 12, datetime.date(2021, 2, 23))
-        expense3 = Expense('spesa cibo', 12, datetime.date(2021, 3, 3))
+        expense1 = Expense("cibo", 40, datetime.date(2021, 2, 14))
+        expense2 = Expense("spesa cibo", 12, datetime.date(2021, 2, 23))
+        expense3 = Expense("spesa cibo", 12, datetime.date(2021, 3, 3))
         tracker.add_expense(expense1)
         tracker.add_expense(expense2)
         tracker.add_expense(expense3)
@@ -25,9 +25,9 @@ class TestTrackYourExpenses(unittest.TestCase):
 
     def test_should_retrieve_expenses_for_a_specific_month_number_3(self):
         tracker = Tracker()
-        expense1 = Expense('cibo', 40, datetime.date(2021, 2, 14))
-        expense2 = Expense('spesa cibo', 12, datetime.date(2021, 2, 23))
-        expense3 = Expense('spesa cibo', 12, datetime.date(2021, 3, 3))
+        expense1 = Expense("cibo", 40, datetime.date(2021, 2, 14))
+        expense2 = Expense("spesa cibo", 12, datetime.date(2021, 2, 23))
+        expense3 = Expense("spesa cibo", 12, datetime.date(2021, 3, 3))
         tracker.add_expense(expense1)
         tracker.add_expense(expense2)
         tracker.add_expense(expense3)
@@ -38,12 +38,25 @@ class TestTrackYourExpenses(unittest.TestCase):
 
     def test_should_return_an_empty_dictionary_if_no_expenses_are_available_for_a_specific_month(self):
         tracker = Tracker()
-        expense1 = Expense('cibo', 40, datetime.date(2021, 2, 14))
-        expense2 = Expense('spesa cibo', 12, datetime.date(2021, 2, 23))
-        expense3 = Expense('spesa cibo', 12, datetime.date(2021, 3, 3))
+        expense1 = Expense("cibo", 40, datetime.date(2021, 2, 14))
+        expense2 = Expense("spesa cibo", 12, datetime.date(2021, 2, 23))
+        expense3 = Expense("spesa cibo", 12, datetime.date(2021, 3, 3))
         tracker.add_expense(expense1)
         tracker.add_expense(expense2)
         tracker.add_expense(expense3)
         expenses = tracker.get_expenses_by_month_and_year(4, 2021)
         self.assertEqual(True, len(expenses.keys()) == 0)
+
+    def test_should_group_expenses_by_category(self):
+        tracker = Tracker()
+        expense1 = Expense("spesa cibo", 40, datetime.date(2021, 2, 14))
+        expense2 = Expense("spesa cibo", 12, datetime.date(2021, 2, 23))
+        expense3 = Expense("benzina", 20, datetime.date(2021, 2, 26))
+        tracker.add_expense(expense1)
+        tracker.add_expense(expense2)
+        tracker.add_expense(expense3)
+        expenses = tracker.get_expenses_by_month_and_year(2, 2021)
+        grouped_expenses = tracker.group_expenses_by_category(expenses)
+        self.assertEqual({"spesa cibo": [expense1, expense2], "benzina": [expense3]}, grouped_expenses)
+
 
